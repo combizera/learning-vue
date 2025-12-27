1d6b23c8cd4b28b1c28749f2710fe34dc4e36ab9
combizera <ycombi349@gmail.com>
2025-12-27 15:45:16 -0300

    feat: deep task-list pt. 1

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="task-item">
+    <span class="task-id">
+      #123
+    </span>
+    <span class="task-status">
+      Concluída
+    </span>
+    <button class="btn-toggle">
+      Concluir
+    </button>
+    <button class="btn-remove">
+      Remover
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.task-item {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  background-color: white;
+  border: 1px solid #ddd;
+  transition: all .3s ;
+}
+.task-id {
+  font-weight: bold;
+  margin-right: 10px;
+  color: #7f8c8d;
+}
+.task-status {
+  flex-grow: 1;
+}
+.btn-toggle {
+  background-color: #f39c12;
+  color: white;
+  margin-right: 5px;
+}
+.btn-toggle:hover {
+  background-color: #e67e22;
+}
+.btn-remove {
+  background-color: #e74c3c;
+  color: white;
+}
+.btn-remove:hover {
+  background-color: #c0392b;
+}
+</style>
